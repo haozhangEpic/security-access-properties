794b5ddae06a1a7e1d95c853292219321eecf818
zhang.hao03 <zhang.hao03@opg.cn>
2023-07-19 15:16:00 +0800

    修复数组路径解析错误

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,20 @@
  * @Author: zhanghao
  * @Date: 2023-07-13 16:31:45
  * @LastEditors: zhanghao
- * @LastEditTime: 2023-07-19 14:41:20
+ * @LastEditTime: 2023-07-19 15:15:16
  * @Description: 
  * @FilePath: \JSON\zhanghaoepic-npm-test001\utils\parsePropetoryPath.js
  */
 // 将属性字符串转换为路径数组
 function parsePropetoryPath(path){
     // 降[]替换为.路径
-    path = path.replace(/[\[ | \]]/g, '.');
+    path = path.replace(/(\[)|(\]\.)/g, '.');
+    // path = path.replace(//g, '.')
     if (path.indexOf('.') === 0) { return [] }
     let pathListByPoint = path.split('.');
+    console.log(path);
     return pathListByPoint
 }
-parsePropetoryPath('adb.adf[0]222');
 
 function findValueByPath(obj, parsePath) {
     let result = obj[parsePath[0]] ;
